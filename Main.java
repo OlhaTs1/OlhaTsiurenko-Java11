42f678beb3e5ee5c94a8aa95cb6915baa7357df3
OlhaTs1 <135816505+OlhaTs1@users.noreply.github.com>
2023-06-07 11:52:32 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("Элементы массива, кратные 3:");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 3 == 0) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+}
